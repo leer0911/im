613c134c5fe70798b0b7a8d5f6fa039c0386af31
lixinxiang <lixinxiang@vv.cn>
2020-04-03 23:30:56 +0800

    feat: 新增demo初始化文件及组件库依赖建立

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # IM
 
-聊天室 Demo
+聊天室 Demo，项目用 Create react app 快速搭建，monorepo 包管理方式。
+
+有自定义的 Eslint 需求的可自行更换，项目默认采用 react-app Eslint 规则。（根目录 Eslint 脚手架自带常规 TS 项目检验规则）
---
+++ packages/app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, CssBaseline } from '@im/component';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Button variant="contained">Learn React</Button>
+    </>
+  );
+}
+
+export default App;
